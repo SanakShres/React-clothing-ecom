943dc47b77a7a0d155358416e36539406a18c21c
SanakShres <sanakstha@gmail.com>
2022-10-25 00:37:50 +0545

    navigating in home route component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+  const navigate = useNavigate();
+
   return (
     <div className="category-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div className="category-body-container">
+      <div
+        className="category-body-container"
+        onClick={() => navigate(`shop/${title}`)}
+      >
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
